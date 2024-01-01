6f0182a47b71a334a5997bbe5f0a163393d810d5
Videars <mvicari989@gmail.com>
2024-01-01 16:33:56 +0100

    website update

--- website/php_scripts/script_get_trips.php
+++ website/php_scripts/script_get_trips.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "Pappagallo99!";
+$password = "**********";
 $dbname = "remora_db";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
